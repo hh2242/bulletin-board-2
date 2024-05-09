9521b3f628136c2144d7dc70671ded2b2e265dba
hh2242 <165195157+hh2242@users.noreply.github.com>
2024-05-09 17:31:32 +0000

    Done

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,7 +6,10 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Board < ApplicationRecord
   validates(:name, presence: true, uniqueness: true)
+
+  belongs_to :user
 end
